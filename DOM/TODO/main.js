907a1c000a77a1728e8f8219461a7cde5d7d81de
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-30 19:44:22 -0500

    Se agregó proyecto TODO (lista de tareas)
    Tiene la funcionalidad actual de agregar al DOM los nuevos elementos creados por el usuario.
    
    Falta manejar los cambios en los nuevos elementos creados (completar, eliminar)

---
+++ DOM/TODO/main.js
@@ -0,0 +1,40 @@
+/* Contextos */ 
+const input = document.getElementById('agregar') //input de texto -- acceder al valor con input.value
+const boton = document.getElementById('crear') // boton
+const lista = document.getElementById('lista-tareas') // contenedor de tareas
+
+function agregarTarea() {
+  if (input.value) { // esta comprobacion devuelve false si es vacía la cadena
+
+    //crear tarea nueva
+    let tareaNueva = document.createElement('div')
+    tareaNueva.classList.add('tarea')
+
+    //texto ingresado por el usuario y agregar a la lista
+    let texto = document.createElement('p')
+    texto.innerText = input.value
+    tareaNueva.appendChild(texto)
+
+    // crear y agregar contenedor de iconos
+    let icon = document.createElement('div')
+    icon.classList.add('iconos')
+    tareaNueva.appendChild(icon)
+
+    // iconos
+    let completar = document.createElement('i')
+    completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar')
+
+    let eliminar = document.createElement('i')
+    eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar')
+
+    icon.append(completar,eliminar) // agrega los iconos al elemento icon
+
+    // Agregar elementos creados a la lista
+    lista.appendChild(tareaNueva)
+  } else {
+    alert('Ingresa una tarea primero')
+  }
+}
+
+//listener para pruebas de desarrollo
+boton.addEventListener('click', agregarTarea )
